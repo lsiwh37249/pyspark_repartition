11983fe6367101fa5a6c9073b5b035aa8dbd390e
lsiwh37249 <lsiwh37249@gmail.com>
2024-08-08 14:00:11 +0900

    test 완료

--- tests/repartition_test.py
+++ tests/repartition_test.py
@@ -2,4 +2,8 @@ from repartition.re_partition import re_partition
 
 def test_repartition():
     size, read_path, write_path = re_partition('20150125', '/data/movie/extract')
+    print(size)
+    print(read_path)
+    print(write_path)
+
     assert True
